875e0ed6d98b8e033fb0ff0bf4d7ac8e5a904423
shankar-1403 <pcred.shankar@gmail.com>
2026-02-21 13:35:49 +0530

    team changes done

--- app/about-us/aboutUs.tsx
+++ app/about-us/aboutUs.tsx
@@ -42,11 +42,12 @@ function AboutUs() {
         {name:"CA Pooja Dubey",designation:"Co-Founder",image:"/assets/team/pooja_dubey.webp"},
         {name:"Ms. Revha Sohani",designation:"Co-Founder",image:"/assets/team/revha.webp"},
         {name:"Mrs. Puja Sharma ",designation:"Promoter",image:"/assets/team/puja_sharma.webp"},
+        {name:"Mr. TakshilKumar Desai",designation:"Senior Partner",image:"/assets/team/takshilkumar_desai.webp"},
+        {name:"Mr. Shreyas Shah",designation:"Senior Partner",image:"/assets/team/shreyas_shah.webp"},
         {name:"Mr. Sanjay Jhanwar",designation:"COO",image:"/assets/team/sanjay_jhanwar.webp"},
         {name:"Mr. Sanam Bhatheja",designation:"Vice President",image:"/assets/team/sanam_batheja.webp"},
         {name:"Mr. Amulya Agarwal",designation:"Assistant Vice President",image:"/assets/team/amulya_agarwal.webp"},
         {name:"Mrs. Priyanka Chauhan",designation:"Corporate Advisor",image:"/assets/team/priyanka.webp"},
-        {name:"Mr. TakshilKumar Desai",designation:"Senior Partner",image:"/assets/team/takshilkumar_desai.webp"},
         {name:"Mr. Pramod Asolkar",designation:"Senior Manager - Operations & Process",image:"/assets/team/pramod_asolkar.webp"},
         {name:"Mrs. Sharon Rahatwal",designation:"Process Manager",image:"/assets/team/sharon.webp"},
     ]
